af2a8e290e720e8e214f74dab4aed764cc5d9bee
adif <muhadifff@gmail.com>
2023-11-05 18:42:59 +0700

    fix: update

--- api-test.sh
+++ api-test.sh
@@ -1,15 +1,15 @@
 # Replace 'YOUR_API_KEY' with your actual Google Maps API key
-API_KEY=env.API_KEY
+API_KEY='AIzaSyDfEzizshW6In3zmHRM1uJdMcaOz3vFLwU'
 
 # Sample coordinates
 origin="-7.360161409,110.5115585"
 destination="-7.363244982,110.4858699"
 
 # Google Maps Distance Matrix API endpoint
-endpoint="https://maps.googleapis.com/maps/api/distancematrix/json"
+endpoint="https://maps.googleapis.com/maps/api/directions/json"
 
 # Construct the request URL
-request_url="${endpoint}?origins=${origin}&destinations=${destination}&mode=driving&language=en-US&units=metric&key=${API_KEY}"
+request_url="${endpoint}?origin=${origin}&destination=${destination}&mode=driving&language=en-US&units=metric&key=${API_KEY}"
 
 # Make the GET request using curl
 curl -X GET "${request_url}"
--- main.py
+++ main.py
@@ -1,64 +1,72 @@
-import aiohttp
 import asyncio
 import csv
+import os
+from datetime import datetime
+from time import sleep
+
+import dotenv
+from dotenv import load_dotenv, dotenv_values
+import gmaps
+import googlemaps
+
+load_dotenv()
+# Replace 'YOUR_GOOGLE_MAPS_API_KEY' with your actual Google Maps API key
+gmaps.configure(api_key=os.getenv('API_KEY'))
+gmaps_client = googlemaps.Client(key=os.getenv('API_KEY'))
 
 
-async def get_travel_time(api_key, session, origin_coord, destination_coord):
-    base_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
-    params = {
-        "origins": f"{origin_coord[0]},{origin_coord[1]}",
-        "destinations": f"{destination_coord[0]},{destination_coord[1]}",
-        "mode": "driving",
-        "key": api_key,
-    }
+def calculate_travel_time(origin, destination):
+    now = datetime.now()
+    directions_result = gmaps_client.directions(origin,
+                                                destination,
+                                                mode="driving",
+                                                departure_time=now)
+    if len(directions_result) == 0:
+        return 0, 0, 0
+    distance = directions_result[0]['legs'][0]['distance']['value']
+    duration = directions_result[0]['legs'][0]['duration']['value']
 
-    async with session.get(base_url, params=params) as response:
-        data = await response.json()
-        print(data)
+    # distance_value = float(data['routes'][0]['legs'][0]['distance']['value'])
+    speed = 0.0 if distance == 0 else distance / duration
 
-        try:
-            travel_time = data["rows"][0]["elements"][0]["duration"]["text"]
-            return travel_time
-        except (KeyError, IndexError):
-            return "N/A"
+    return distance, duration, speed * 3.6
 
 
-def read_coordinates_from_csv(csv_file):
-    coordinates = []
-    with open(csv_file, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        header = next(reader)  # Skip the header
-        for row in reader:
-            kecamatan, kelurahan, xcoord, ycoord = row
-            coordinates.append((float(xcoord), float(ycoord), kelurahan))
-    return coordinates
+async def process_combinations(writer, origin_data, destination_data):
+    origin = f"{origin_data[2]},{origin_data[3]}"
+    destination = f"{destination_data[2]},{destination_data[3]}"
 
+    distance, travel_time, speed = calculate_travel_time(origin, destination)
 
-async def generate_combination_csv(api_key, coordinates, output_file):
-    async with aiohttp.ClientSession() as session:
-        with open(output_file, "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, delimiter=';')
-            header = ["Kelurahan"] + [coord[2] for coord in coordinates]
-            writer.writerow(header)
+    writer.writerow({
+        'Kelurahan Origin': origin_data[1],
+        'Kelurahan Destination': destination_data[1],
+        'Distance (meter)': distance,
+        'Duration in  Traffic (second)': travel_time,
+        'Speed (km/h)': speed
+    })
+async def main():
+    # Read data from CSV file
+    with open('source.csv', 'r') as csvfile:
+        data = csv.DictReader(csvfile)
+        coordinates = [tuple(row.values()) for row in data]
 
-            for origin_coord in coordinates:
-                row = [origin_coord[2]]
+    # Initialize the output CSV file
+    with open('output_result.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Kelurahan Origin', 'Kelurahan Destination', 'Distance (meter)', 'Duration in  Traffic (second)',
+                      'Speed (km/h)']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-                tasks = [get_travel_time(api_key, session, (origin_coord[0], origin_coord[1]),
-                                         (dest_coord[0], dest_coord[1])) for dest_coord in coordinates]
-
-                results = await asyncio.gather(*tasks)
-
-                row.extend(results)
-                writer.writerow(row)
+        for origin_data in coordinates:
+            process = []
+            for destination_data in coordinates:
+                print(f"procees {origin_data[1]} - {destination_data[1]}")
+                process_async = process_combinations(writer, origin_data, destination_data)
+                process.append(process_async)
+            await asyncio.gather(*process)
+            print(f"done {origin_data[1]}")
 
 
 if __name__ == "__main__":
-    api_key = env.API_KEY
-    input_csv_file = "source.csv"
-
-    coordinates = read_coordinates_from_csv(input_csv_file)
-
-    output_file = "travel_time_matrix.csv"
-
-    asyncio.run(generate_combination_csv(api_key, coordinates, output_file))
+    asyncio.run(main())
